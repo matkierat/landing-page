59d59631589c9696204b8b8624641fc2ffff80c5
matkierat <mat.kierat@gmail.com>
2026-09-08 17:26:34 +0200

    Dodanie Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Manrope, Raleway } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const manrope = Manrope({
@@ -47,6 +48,15 @@ export default function RootLayout({
   return (
     <html lang="pl" className={`${manrope.variable} ${raleway.variable}`}>
       <body>{children}</body>
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "yf5kctfkb0");
+        `}
+      </Script>
     </html>
   );
 }
